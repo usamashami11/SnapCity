46e3544312c969344dc6dd43e8ec537ff280a7c0
Syeda-Umaima <syedaumaima59@gmail.com>
2026-06-03 01:47:56 +0500

    security: remove hardcoded API key from AndroidManifest.xml and load from local.properties

--- frontend/android/app/build.gradle.kts
+++ frontend/android/app/build.gradle.kts
@@ -1,3 +1,13 @@
+import java.util.Properties
+
+val localProperties = Properties().apply {
+    val localPropertiesFile = rootProject.file("local.properties")
+    if (localPropertiesFile.exists()) {
+        localPropertiesFile.inputStream().use { load(it) }
+    }
+}
+val googleMapsApiKey = localProperties.getProperty("MAPS_API_KEY") ?: System.getenv("GOOGLE_MAPS_API_KEY") ?: ""
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
@@ -28,6 +38,7 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        manifestPlaceholders["mapsApiKey"] = googleMapsApiKey
     }
 
     buildTypes {
